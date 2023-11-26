49ace7c643cdea99d06e24189eb5007c48a435d7
Christopher Hardy Gunawan <christopher.hygunawan@gmail.com>
2023-11-26 15:00:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Smooth Scrolling
 document.querySelectorAll('.link').forEach(item => {
     item.addEventListener('click', function (e) {
-      e.preventDefault(); // Prevent the default behavior of the anchor tag
+      e.preventDefault(); 
       
-      const target = this.getAttribute('href').substring(1); // Get the target section ID
+      const target = this.getAttribute('href').substring(1); 
       scrollToSection(target);
     });
   });
@@ -16,18 +16,14 @@ document.querySelectorAll('.link').forEach(item => {
   }
 
 // Hamburger Menu
-// Select the mobile menu elements
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuIcon = document.querySelector('.menu-icon');
 const mobileNav = document.querySelector('.mobile-nav');
 
-// Add an event listener to the menu icon
 menuIcon.addEventListener('click', () => {
-    // Toggle the 'show' class on the mobile menu
     mobileNav.classList.toggle('show');
 });
 
-// Add an event listener to close the menu when a menu item is clicked
 const mobileNavItems = mobileNav.querySelectorAll('li');
 mobileNavItems.forEach((item) => {
     item.addEventListener('click', () => {
@@ -91,43 +87,3 @@ productContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
-
-// Aside Sidebar
-const dots = document.querySelectorAll('.dot');
-
-function setActiveDot(sectionId) {
-    dots.forEach((dot) => {
-        dot.classList.remove('active');
-    });
-    const dot = document.querySelector(`[data-section="${sectionId}"]`);
-    if (dot) {
-        dot.classList.add('active');
-    }
-}
-
-window.addEventListener('scroll', () => {
-    const scrollY = window.scrollY;
-    const sectionOffsets = {
-        'hero-section': 0,
-        'about-us-section': 500,
-        'destination-section': 1000,
-        'testimonials-section': 1500,
-        'contact-us-section': 2000,
-    };
-
-    for (const section in sectionOffsets) {
-        if (scrollY >= sectionOffsets[section]) {
-            setActiveDot(section);
-        }
-    }
-});
-
-dots.forEach((dot) => {
-    dot.addEventListener('click', () => {
-        const sectionId = dot.getAttribute('data-section');
-        const section = document.getElementById(sectionId);
-        if (section) {
-            section.scrollIntoView({ behavior: 'smooth' });
-        }
-    });
-});
