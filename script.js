bc37268097c343f66cf532596234d2c8b3e2c0c6
Christopher Hardy Gunawan <christopher.hygunawan@gmail.com>
2023-11-26 19:45:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -87,3 +87,21 @@ productContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
+
+// Hero Section Slider
+const carouselContainers = [...document.querySelectorAll('.hero-carousel')];
+const nxtBtn1 = [...document.querySelectorAll('.nxt-btn1')];
+const preBtn1 = [...document.querySelectorAll('.pre-btn1')];
+
+carouselContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn1[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn1[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
